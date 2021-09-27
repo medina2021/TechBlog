ccafcc80867d3c21f2a992c58d5923720c55ca4a
Ever <evermedina2025@gmail.com>
2021-09-26 18:33:02 -0700

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: 'tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306,
   }
